9dec2e9c9f727cb8bfb8deee441286dc9e1f11df
sam-champion <sam.champs@hotmail.com>
2024-12-09 10:11:20 +0000

    initial styling of background gradient

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -35,7 +35,7 @@ function Login() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=blue&shade=500"
+            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
             alt="Byte Size Trivia"
           />
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-800">
@@ -60,7 +60,7 @@ function Login() {
                   autoComplete="email"
                   required
                   onChange={(e) => setEmail(e.target.value)}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-blue-400 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 ></input>
               </div>
             </div>
@@ -75,7 +75,7 @@ function Login() {
                 </label>
                 <Link
                   to="/resetpassword"
-                  className="text-sm font-semibold text-blue-500 hover:text-blue-400"
+                  className="text-sm font-semibold text-indigo-600 hover:text-indigo-500"
                 >
                   Forgot password?
                 </Link>
@@ -88,7 +88,7 @@ function Login() {
                   autoComplete="current-password"
                   required
                   onChange={(e) => setPassword(e.target.value)}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-blue-400 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                 ></input>
               </div>
             </div>
@@ -96,7 +96,7 @@ function Login() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
@@ -106,7 +106,7 @@ function Login() {
             Don't have an account?
             <Link
               to="/register"
-              className="font-semibold text-blue-500 hover:text-blue-400"
+              className="font-semibold text-indigo-600 hover:text-indigo-500"
             >
               {" "}
               Sign up here.
